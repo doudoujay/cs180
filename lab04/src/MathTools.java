7613ea6a21315d1267ad4a3bf0cc210ef9f95885
Jay Ma <jay@pal-nat186-145-220.itap.purdue.edu>
2017-01-31 13:42:27 -0500

    A

--- lab04/src/MathTools.java
+++ lab04/src/MathTools.java
@@ -13,10 +13,10 @@ public class MathTools {
         double result = 1;
         if (exponent < 0) {
             double abs = absoluteValue(exponent);
-            for (int i = 0; i < exponent; i++) {
+            for (int i = 0; i < abs; i++) {
                 result = result * base;
             }
-            result = 1/result;
+            result = (double) 1.0 / result;
         } else {
             for (int i = 0; i < exponent; i++) {
                 result = result * base;
@@ -26,12 +26,63 @@ public class MathTools {
     }
 
     public static double nthRoot(double value, int root) {
-        double result = 0;
-        if (root>0){
 
+        if (root == 0 || value <= 0) {
+            return 0;
+        } else {
+            double initialGuess = value;
+            if (root < 0) {
+                initialGuess = (double) 1 / value;
+            }
+            double nextGuess = calculateNext(root, initialGuess, value);
+            double change = nextGuess - initialGuess;
+            while (change > 0.000000001) {
+                initialGuess = nextGuess;
+                nextGuess = calculateNext(root, initialGuess, value);
+                change = nextGuess - initialGuess;
+            }
+            return nextGuess;
         }
 
 
-        return result;
+    }
+
+    public static String scientificNotation(double n) {
+        double coef = n;
+        int exponent = 0;
+        double abs = absoluteValue(n);
+
+        if (abs < 1) {
+            while (findSpaces(n) < 6) {
+                exponent = exponent - 1;
+                coef = coef * 10;
+            }
+            return (n + " x 10 ^ " + exponent);
+        }
+
+        if (abs >= 10) {
+            while (findSpaces(n) < 6) {
+                exponent = exponent + 1;
+                coef = coef / 10;
+
+            }
+            return (n + " x 10 ^ " + exponent);
+
+        }
+
+        return (n + " x 10 ^ " + exponent);
+
+
+    }
+
+    public static int findSpaces(double n) {
+        String text = Double.toString(Math.abs(n));
+        int integerPlaces = text.indexOf('.');
+        int decimalPlaces = text.length() - integerPlaces - 1;
+        return decimalPlaces;
+    }
+
+    public static double calculateNext(int n, double initialGuess, double value) {
+        return (double) ((double) (n - 1) * initialGuess + value / power(initialGuess, (n - 1))) / (double) n;
     }
 }
--- lab04/src/MathToolsClient.java
+++ lab04/src/MathToolsClient.java
@@ -1,9 +1,49 @@
+import java.util.Scanner;
 
 /**
  * Created by jay on 1/31/17.
  */
 public class MathToolsClient {
     public static void main(String[] args) {
-        System.out.print(MathTools.absoluteValue(-1));
+        Scanner input = new Scanner(System.in);
+        boolean bye = false;
+        while (bye) {
+            System.out.println("Select a number from the menu choices:");
+            System.out.println("1 - Absolute Value");
+            System.out.println("2 - Power");
+            System.out.println("3 - Nth Root");
+            System.out.println("4 - Scientific Notation");
+            switch(input.nextLine()){
+                case "1":
+                    System.out.println("Enter the value");
+                    double n= Integer.parseInt(input.nextLine());
+                    System.out.println(MathTools.absoluteValue(n));
+
+                    default:
+                        System.out.println("Invalid");
+                case "2":
+                    double base= Integer.parseInt(input.nextLine());
+                    int exponent= Integer.parseInt(input.nextLine());
+                    System.out.println(MathTools.power(base,exponent));
+
+            }
+            System.out.println("Return to menu? yes/no");
+            switch (input.nextLine()){
+                case "yes":
+                    bye = false;
+                case "no":
+                    bye = true;
+            }
+
+
+        }
+        System.out.println("Select a number from the menu choices:");
+        System.out.println("1 - Absolute Value");
+        System.out.println("2 - Power");
+        System.out.println("3 - Nth Root");
+        System.out.println("4 - Scientific Notation");
+        System.out.println(MathTools.nthRoot(4,2));
     }
+
 }
+
