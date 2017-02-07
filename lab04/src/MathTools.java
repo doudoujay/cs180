757d0580d7635af95ac4f7a4582425300e59bffc
Jay Ma <jay@pal-nat186-157-89.itap.purdue.edu>
2017-02-07 12:25:08 -0500

    fixed the lab04

--- lab04/src/MathTools.java
+++ lab04/src/MathTools.java
@@ -16,7 +16,7 @@ public class MathTools {
             for (int i = 0; i < abs; i++) {
                 result = result * base;
             }
-            result = (double) 1.0 / result;
+            result = 1.0 / result;
         } else {
             for (int i = 0; i < exponent; i++) {
                 result = result * base;
@@ -29,47 +29,52 @@ public class MathTools {
 
         if (root == 0 || value <= 0) {
             return 0;
-        } else {
-            double x1 = value;
-            double x2 = value / root;
-            while (Math.abs(x1 - x2) > 0.000000001)
-            {
-                x1 = x2;
-                x2 = ((root- 1.0) * x2 + value / Math.pow(x2, root - 1.0)) / root;
-            }
-            return x2;
         }
+        double initialGuess = 0;
+        if (root > 0)
+            initialGuess = value;
+        else if (root < 0)
+            initialGuess = (double) 1 / value;
+
+
+        double nextGuess = (1.0 / root) * ((root - 1) * initialGuess + value / power(initialGuess, root - 1));
+        while (absoluteValue(initialGuess - nextGuess) > 0.000000000001) {
+            initialGuess = nextGuess;
+            nextGuess = (1.0 / root) * ((root - 1) * initialGuess + value / power(initialGuess, root - 1));
+        }
+        return nextGuess;
 
 
     }
 
 
     public static String scientificNotation(double n) {
-        double coef = n;
-        int exponent = 0;
-        double abs = absoluteValue(n);
-        int a = findSpaces(coef);
-        if (abs < 1) {
-
-           while (a <= 6){
-                exponent = exponent - 1;
+        if (n < 0) {
+            n = MathTools.absoluteValue(n);
+        }
+        if (n < 1) {
+            int i = 0;
+            while (n < 1) {
                 n = n * 10;
-                a--;
+                i++;
             }
-            return (coef + " x 10 ^ " + exponent);
-        }
-
-        if (abs >= 10) {
-            while (a <= 6) {
-                exponent = exponent + 1;
-                coef = coef / 10;
-                a++;
+            n = ((int) (n * 1000000)) / 1000000.0;
+            String ans = (n + " x 10 ^ " + "-" + i);
+            return ans;
+        } else if (n >= 10) {
+            int i = 0;
+            while (n >= 10) {
+                n = n / 10;
+                i++;
             }
-            return (coef + " x 10 ^ " + exponent);
-
+            n = ((int) (n * 1000000)) / 1000000.0;
+            String ans = (n + " x 10 ^ " + i);
+            return ans;
+        } else {
+            n = ((int) (n * 1000000)) / 1000000.0;
+            String ans = n + " x 10 ^ 0";
+            return ans;
         }
-
-        return (coef + " x 10 ^ " + exponent);
 
 
     }
@@ -81,7 +86,5 @@ public class MathTools {
         return decimalPlaces;
     }
 
-    public static double calculateNext(int n, double initialGuess, double value) {
-        return (double) ((double) (n - 1) * initialGuess + value / power(initialGuess, (n - 1))) / (double) n;
-    }
+
 }
